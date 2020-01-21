8d56f24ede7c79cb7f252ab2183b0b4a3cb4a5eb
Mateo Caraffo <47824441+mateocaraffo@users.noreply.github.com>
2020-01-21 12:08:27 -0300

    Update actionTypes.js

--- src/players/actionTypes.js
+++ src/players/actionTypes.js
@@ -2,4 +2,3 @@ export const FETCH = 'players/FETCH';
 export const FETCH_START = 'players/START';
 export const FETCH_SUCCESS = 'players/SUCCESS';
 export const FETCH_FAIL = 'players/FAIL';
-export const FILTER = 'players/FILTER';
